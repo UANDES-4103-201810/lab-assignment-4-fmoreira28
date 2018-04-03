66ae7cfba92433396e7c3d39307df128f45822ac
Web <fjmoreira@miuandes.cl>
2018-04-03 13:20:00 -0300

    more validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :place
   has_many :tickets
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
+
 end
