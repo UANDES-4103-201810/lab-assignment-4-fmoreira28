13e04302fd48bce21e5a88f0e13c6883621d964c
Web <fjmoreira@miuandes.cl>
2018-04-03 13:02:08 -0300

    Validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,5 @@ class Ticket < ApplicationRecord
   belongs_to :event
   has_many :user_tickets
   has_many :users, through: :user_tickets
-
+  validates :price, greater_than:0,:integer=>true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   	has_many :user_tickets	
-	has_many :tickets, :through => :user_tickets
+		has_many :tickets, :through => :user_tickets
+		validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+		validates :phone,:presence => true,
+							:numericality => true,
+							:length => { :minimum => 9, :maximum => 12 }
+		validates :password, with: /^[A-Za-z0-9]+$/,
+							:length => { :minimum => 8, :maximum => 12 }
 end
